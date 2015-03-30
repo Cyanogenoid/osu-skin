c3eb1a2ff0581764ad479ed5132ba8870acb601d
Yan Zhang <cyanogenoid@cyanogenoid.com>
2015-03-30 13:57:29 +0200

    Support wildcards in config.yaml

--- make.py
+++ make.py
@@ -14,10 +14,15 @@ def load_config():
     with open('config.yaml') as fd:
         return yaml.load(fd)
 
-def make_rule(target, dependency, commands):
-    source_path = os.path.join(SOURCE_DIR, dependency)
-    target_path = os.path.join(TARGET_DIR, target)
-    cmd_string  = '\n\t'.join(commands).format(src=source_path, tgt=target_path)
+def make_rule(target, dependency, commands, *, wildcard=False):
+    if not wildcard:
+        source_path = os.path.join(SOURCE_DIR, dependency)
+        target_path = os.path.join(TARGET_DIR, target)
+        cmd_string  = '\n\t'.join(commands).format(src=source_path, tgt=target_path)
+    else:
+        source_path = os.path.join(SOURCE_DIR, dependency.replace('*', '%'))
+        target_path = os.path.join(TARGET_DIR, target.replace('*', '%'))
+        cmd_string  = '\n\t'.join(commands).format(src='$<', tgt='$@')
     return Rule(target_path, source_path, cmd_string)
 
 def process_svg(target, r):
@@ -39,7 +44,7 @@ def process_svg(target, r):
     coms = ['inkscape -f {{src}} -e {{tgt}} {}'.format(' '.join(params))]
     if r.get('fix', False):
         coms.append('python fix-antialias.py {{tgt}}'.format())
-    r = make_rule(rule, dep, coms)
+    r = make_rule(rule, dep, coms, wildcard=('*' in rule))
     yield r
     for name in names[1:]:
         name = os.path.join(TARGET_DIR, name + '@2x.png')
@@ -86,10 +91,17 @@ def process_config(config):
     for target, r in config.get('ini', {}).items():
         yield process_ini(target, r)
 
+def collect_all_rule(rules):
+    targets  = [r.rule for r in rules if '%' not in r.rule]
+    for r in rules:
+        if '%' in r.rule:
+            targets.append('$(wildcard {})'.format(r.rule.replace('%', '*')))
+    return Rule('all', ' '.join(targets), '')
+
 
 config = load_config()
 rules = list(process_config(config))
-rules.append(Rule('all', ' '.join(r.rule for r in rules), ''))
+rules.append(collect_all_rule(rules))
 with open('Makefile', 'w') as fd:
     for r in reversed(rules):
         fd.write('{}: {}\n\t{}'.format(r.rule, ' '.join([r.deps, '| bin/']), r.coms))
