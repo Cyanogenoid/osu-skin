87835306fe771a18a88d727cf492f7e569b292d6
Yan Zhang <cyanogenoid@cyanogenoid.com>
2015-03-30 02:03:31 +0200

    Split process_config into more methods

--- make.py
+++ make.py
@@ -20,61 +20,71 @@ def make_rule(target, dependency, commands):
     cmd_string  = '\n\t'.join(commands).format(src=source_path, tgt=target_path)
     return Rule(target_path, source_path, cmd_string)
 
+def process_svg(target, r):
+    try:
+        names = r['names']
+    except KeyError:
+        names = [target]
+    rule = names[0] + '@2x.png'
+    dep  = target + '.svg'
+    params = []
+    try:
+        params.append('-w {}'.format(r['w']))
+    except KeyError:
+        pass
+    try:
+        params.append('-h {}'.format(r['h']))
+    except KeyError:
+        pass
+    coms = ['inkscape -f {{src}} -e {{tgt}} {}'.format(' '.join(params))]
+    if r.get('fix', False):
+        coms.append('python fix-antialias.py {{tgt}}'.format())
+    r = make_rule(rule, dep, coms)
+    yield r
+    for name in names[1:]:
+        name = os.path.join(TARGET_DIR, name + '@2x.png')
+        com = 'cp {} {}'.format(dep, name)
+        yield Rule(name, r.rule, com)
+
+def process_png(target, r):
+    try:
+        names = r['names']
+    except KeyError:
+        names = [target]
+    for name in names:
+        rule = name   + '@2x.png'
+        dep  = target + '.png'
+        coms = ['cp {{src}} {{tgt}}'.format()]
+        return make_rule(rule, dep, coms)
+
+def process_ini(target, r):
+    rule = target + '.ini'
+    dep  = rule
+    coms = ['cp {{src}} {{tgt}}'.format()]
+    return make_rule(rule, dep, coms)
+
+def process_py(target, r):
+    target_path = os.path.join(SOURCE_DIR, target + '.py')
+    deps = [os.path.join(SOURCE_DIR, s) for s in r['srcs']]
+    deps.append(target_path)
+    coms = 'python {}'.format(target_path)
+    try:
+        names = r['names']
+    except KeyError:
+        names = [target]
+    for name in names:
+        name = os.path.join(TARGET_DIR, name + '@2x.png')
+        yield Rule(name, ' '.join(deps), coms)
+
 def process_config(config):
     for target, r in config.get('svg', {}).items():
-        try:
-            names = r['names']
-        except KeyError:
-            names = [target]
-
-        rule = names[0] + '@2x.png'
-        dep  = target + '.svg'
-        params = []
-        try:
-            params.append('-w {}'.format(r['w']))
-        except KeyError:
-            pass
-        try:
-            params.append('-h {}'.format(r['h']))
-        except KeyError:
-            pass
-        coms = ['inkscape -f {{src}} -e {{tgt}} {}'.format(' '.join(params))]
-        if r.get('fix', False):
-            coms.append('python fix-antialias.py {{tgt}}'.format())
-        r = make_rule(rule, dep, coms)
-        yield r
-
-        for name in names[1:]:
-            name = os.path.join(TARGET_DIR, name + '@2x.png')
-            com = 'cp {} {}'.format(dep, name)
-            yield Rule(name, r.rule, com)
-    for target, r in config.get('png', {}).items():
-        try:
-            names = r['names']
-        except KeyError:
-            names = [target]
-        for name in names:
-            rule = name   + '@2x.png'
-            dep  = target + '.png'
-            coms = ['cp {{src}} {{tgt}}'.format()]
-            yield make_rule(rule, dep, coms)
-    for target, r in config.get('ini', {}).items():
-        rule = target + '.ini'
-        dep  = rule
-        coms = ['cp {{src}} {{tgt}}'.format()]
-        yield make_rule(rule, dep, coms)
+        yield from process_svg(target, r)
     for target, r in config.get('py', {}).items():
-        target_path = os.path.join(SOURCE_DIR, target + '.py')
-        deps = [os.path.join(SOURCE_DIR, s) for s in r['srcs']]
-        deps.append(target_path)
-        coms = 'python {}'.format(target_path)
-        try:
-            names = r['names']
-        except KeyError:
-            names = [target]
-        for name in names:
-            name = os.path.join(TARGET_DIR, name + '@2x.png')
-            yield Rule(name, ' '.join(deps), coms)
+        yield from process_py(target, r)
+    for target, r in config.get('png', {}).items():
+        yield process_png(target, r)
+    for target, r in config.get('ini', {}).items():
+        yield process_ini(target, r)
 
 
 config = load_config()
