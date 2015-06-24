fe2968193ce75c6d79724f53da543409fd93123e
Yan Zhang <cyanogenoid@cyanogenoid.com>
2015-06-24 16:53:28 +0100

    Only optimise optimisable svgs

--- src/optimise.sh
+++ src/optimise.sh
@@ -1,9 +1,13 @@
 OPTIONS='--enable-id-stripping --enable-comment-stripping --shorten-ids --remove-metadata'
 
 for f in *.svg; do
-  if [[ ! $f == "selection-mod-"* ]]; then
-    scour $OPTIONS -i $f -o $f.new
-    rm $f
-    mv $f.new $f
+  if [[ ! $f == "selection-mod- "* ]]; then
+    scour $OPTIONS -i "$f" -o "$f.new"
+    if [[ $(wc -c < "$f.new") -lt $(wc -c < "$f") ]]; then
+	  rm $f
+	  mv $f.new $f
+	else
+	  rm $f.new
+	fi
   fi
 done
