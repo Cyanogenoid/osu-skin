703c9c7a70ab0f61825d3af47713a3eb5d7cef26
Yan Zhang <cyanogenoid@cyanogenoid.com>
2015-06-24 16:57:05 +0100

    Only print optimised svgs when optimising

--- src/optimise.sh
+++ src/optimise.sh
@@ -1,11 +1,12 @@
 OPTIONS='--enable-id-stripping --enable-comment-stripping --shorten-ids --remove-metadata'
 
 for f in *.svg; do
-  if [[ ! $f == "selection-mod- "* ]]; then
-    scour $OPTIONS -i "$f" -o "$f.new"
+  if [[ ! $f == "selection-mod-"* ]]; then
+    scour $OPTIONS --quiet -i "$f" -o "$f.new"
     if [[ $(wc -c < "$f.new") -lt $(wc -c < "$f") ]]; then
 	  rm $f
 	  mv $f.new $f
+	  echo Optimised $f.
 	else
 	  rm $f.new
 	fi
