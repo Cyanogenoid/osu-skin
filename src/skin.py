de102ba739fd9647ff264bd4eb27a39454d36db8
Yan Zhang <cyanogenoid@cyanogenoid.com>
2015-08-27 17:21:06 +0100

    Use intermediate yaml config in skin.py

--- src/skin.py
+++ src/skin.py
@@ -1,40 +1,49 @@
 import sys
 
-with open(sys.argv[0], 'w') as fd:
-    fd.write('''
-[General]
-
-Name: Cyanogenoid 0.1
-Author: Cyanogenoid
-Version: 2.5
-
-CursorRotate: 0
-CursorExpand: 0
-CursorCentre: 1
+import yaml
 
 
-[Colours]
+config_yaml = '''
+- General:
+    Name: Cyanogenoid 0.1
+    Author: Cyanogenoid
+    Version: 2.5
 
-Combo1: 255,255,255
-Combo2: 0,162,255
-
-SliderTrackOverride: 0,0,0
-SliderBorder: 91,91,91
-
-SongSelectActiveText: 255,255,255
-SongSelectInactiveText: 192,192,192
-
-MenuGlow: 255,255,255
+    CursorRotate: 0
+    CursorExpand: 0
+    CursorCentre: 1
 
 
-[Fonts]
+- Colours:
+    Combo1: 255, 255, 255
+    Combo2: 0, 162, 255
 
-HitCirclePrefix: default
-HitCircleOverlap: 159
+    SliderTrackOverride: 0, 0, 0
+    SliderBorder: 91, 91, 91
 
-ComboPrefix: combo
-ComboOverlap: 0
+    SongSelectActiveText: 255, 255, 255
+    SongSelectInactiveText: 192, 192, 192
 
-ScorePrefix: score
-ScoreOverlap: 0
-    ''')
+    MenuGlow: 255, 255, 255
+
+
+- Fonts:
+    HitCirclePrefix: default
+    HitCircleOverlap: 159
+
+    ComboPrefix: combo
+    ComboOverlap: 0
+
+    ScorePrefix: score
+    ScoreOverlap: 0
+'''
+
+config = yaml.load(config_yaml)
+
+
+with open(sys.argv[1], 'w') as fd:
+    for key in config:
+        key_name, key_content = next(iter(key.items()))
+        fd.write('[{}]\n'.format(key_name))
+        for option, value in key_content.items():
+            fd.write('{}: {}\n'.format(option, value))
